a1d4df3dbfb7f454167182d8ce7d59a73f6fe0de
brkyozdemir <berkayozdemir02@gmail.com>
2020-08-17 18:38:02 +0300

    logging_and_endcheck_is_done

--- Visilabs.Log.Replace/Program.cs
+++ Visilabs.Log.Replace/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
+using System.Configuration;
+using System.Diagnostics.Tracing;
 using System.IO;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using StreamWriter = System.IO.StreamWriter;
 
 namespace Visilabs.Log.Replace
 {
     class Program
     {
+        private static string corrupted;
+        private static string modify;
         static void Main(string[] args)
         {
+            var appSettings = ConfigurationManager.AppSettings;
+            corrupted = appSettings["Visilabs"];
+            modify = appSettings["Modify"];
 
-            string folderPath = "C:\\corruptedLogs";
-
-            DirectoryInfo startDir = new DirectoryInfo(folderPath);
+            DirectoryInfo startDir = new DirectoryInfo(corrupted);
 
             RecurseFileStructure recurseFileStructure = new RecurseFileStructure();
             recurseFileStructure.TraverseDirectory(startDir);
@@ -30,12 +36,17 @@ namespace Visilabs.Log.Replace
                 }
 
                 var files = directoryInfo.EnumerateFiles();
-
+                int counter = 0;
                 foreach (var file in files)
                 {
-                    HandleFile(file);
+                    if (file.Name.EndsWith(".log"))
+                    {
+                        counter += 1;
+                        var percentage = (counter * 100) / files.Count();
+                        HandleFile(file);
+                        Console.WriteLine("%" + percentage + " of files is done.");
+                    }
                 }
-
             }
 
             void HandleFile(FileInfo file)
@@ -43,27 +54,27 @@ namespace Visilabs.Log.Replace
                 Console.WriteLine("{0}", file.Name);
                 try
                 {
-                    string[] lines = File.ReadAllLines("C:\\corruptedLogs\\"+file.Name);
-                    using (StreamWriter writer = new StreamWriter("C:\\Modify\\"+file.Name))
+                    string[] lines = File.ReadAllLines(corrupted + file.Name);
+                    using (StreamWriter writer = new StreamWriter(modify + file.Name))
                     {
-                        for (int i = 0; i < lines.Length; i++)
+                        Console.WriteLine("Process has started...");
+                        for (int i = 1; i < lines.Length - 1; i++)
                         {
                             if (lines[i].Contains("OM.tid"))
                             {
+                                var percentage = (i * 100) / lines.Count();
+                                Console.WriteLine(i + "th line was corrupted at %" + percentage + " and recovered.");
                                 lines[i] = lines[i].Replace("OM.pb", "OM.pp");
                             }
                             writer.WriteLine(lines[i]);
                         }
+                        Console.WriteLine("Fully healed.");
                         writer.Flush();
                     }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Exception: " + e.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("Executing finally block.");
                 }
             }
         }
